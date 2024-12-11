6e5367b9d48510107397d6dc87b372ca993086da
kienhoang3397 <125807253+kienhoang3397@users.noreply.github.com>
2024-12-11 14:49:14 +0700

    fix: adjust positioning of ConnectWallet component in MainLayout for better visibility and layout consistency

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -23,8 +23,8 @@ export function MainLayout({ children, className }: MainLayoutProps) {
             {/* Fixed Header */}
             <Navbar />
 
-       
-            <div className="sticky top-24 h-0 right-4 z-50 flex justify-end">
+
+            <div className="sticky top-4 h-0 right-12 z-50 flex justify-end pr-4">
                 <ConnectWallet />
             </div>
 
