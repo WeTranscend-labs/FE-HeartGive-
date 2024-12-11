a9cae650698f668e32d5ab60b8a00e9e1bbd8b99
kienhoang3397 <125807253+kienhoang3397@users.noreply.github.com>
2024-12-11 13:45:28 +0700

    refactor: replace layout structure in App component by integrating MainLayout, streamlining the overall application layout and improving code organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
+import { MainLayout } from './layouts/MainLayout';
 import { HomePage } from './pages/HomePage';
 import { RegisterPage } from './pages/RegisterPage';
 import { FundDetailsPage } from './pages/FundDetailsPage';
 import { LandingPage } from './pages/LandingPage';
 import { AboutPage } from './pages/AboutPage';
-import { Navbar } from './components/Navbar';
-import { Footer } from './components/Footer';
 import ContextProvider from './contexts';
 
 function App() {
   return (
     <ContextProvider>
       <Router>
-        <div className="min-h-screen bg-gray-50 flex flex-col">
-          <Toaster position="top-right" />
-          <Navbar />
-          <div className="h-20">{/* Spacer for fixed navbar */}</div>
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<LandingPage />} />
-              <Route path="/funds" element={<HomePage />} />
-              <Route path="/register" element={<RegisterPage />} />
-              <Route path="/fund/:id" element={<FundDetailsPage />} />
-              <Route path="/about" element={<AboutPage />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
+        <MainLayout>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/funds" element={<HomePage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/fund/:id" element={<FundDetailsPage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </MainLayout>
       </Router>
     </ContextProvider>
   );
